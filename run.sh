93ccf627226f4cbd6a140d10decd2e0072475df3
MyNameIsBBB <peeratus0912@gmail.com>
2026-06-07 17:50:10 +0700

    feat: update

--- run.sh
+++ run.sh
@@ -31,6 +31,12 @@ docker builder prune -f
 echo "=== Building and starting docker containers ==="
 $DOCKER_CMD up -d --build
 
+echo "=== Waiting for MongoDB to start ==="
+sleep 5
+
+echo "=== Initializing MongoDB Replica Set ==="
+docker exec tinyschedule-mongo mongo --eval "rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'localhost:27017'}]})" || true
+
 echo "=== Starting Tailscale Funnel on Port 3000 ==="
 nohup sudo tailscale funnel 3000 > tailscale_funnel.log 2>&1 &
 echo "Tailscale Funnel is running in background. Logs saved to tailscale_funnel.log https://tinyschedule.taile459d4.ts.net/"
