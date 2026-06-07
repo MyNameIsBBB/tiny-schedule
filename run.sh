b79424bcc1ecb9034a94655c92ae46678fa51080
MyNameIsBBB <peeratus0912@gmail.com>
2026-06-07 14:39:03 +0700

    feat: add run sh

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Detect docker compose or docker-compose command
+if command -v docker compose &> /dev/null; then
+  DOCKER_CMD="docker compose"
+elif command -v docker-compose &> /dev/null; then
+  DOCKER_CMD="docker-compose"
+else
+  echo "Error: Neither 'docker compose' nor 'docker-compose' could be found."
+  exit 1
+fi
+
+echo "=== Stopping current Tailscale Funnel ==="
+sudo pkill -f "tailscale funnel" || true
+
+echo "=== Pulling latest changes from git ==="
+git pull
+
+echo "=== Stopping current docker containers ==="
+$DOCKER_CMD down
+
+echo "=== Removing stopped docker containers ==="
+$DOCKER_CMD rm -f
+
+echo "=== Building and starting docker containers ==="
+$DOCKER_CMD up -d --build
+
+echo "=== Starting Tailscale Funnel on Port 3000 ==="
+nohup sudo tailscale funnel 3000 > tailscale_funnel.log 2>&1 &
+echo "Tailscale Funnel is running in background. Logs saved to tailscale_funnel.log https://ubuntu-homelab.taile459d4.ts.net/"
+
+echo "=== Done ==="
